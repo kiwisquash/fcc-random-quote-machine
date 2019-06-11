f90c2a50cc6225a9d1ad086aadb1c9f68a50fba0
Ki Song <kiwisquash@gmail.com>
2019-06-10 23:51:26 -0700

    Added function to extract json from FCC's quote list and pick a random one.

--- site/src/index.js
+++ site/src/index.js
@@ -1,5 +1,30 @@
+let quoteData;
+
+function getQuotes() {
+  return $.getJSON("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+    (jsonQuotes) => {
+			quoteData = jsonQuotes;
+			console.log(quoteData);
+    }
+  );
+}
+
+function getQuote() {
+	let num = quoteData.quotes.length;
+	let randQuote = quoteData.quotes[Math.floor(Math.random() * num)];
+	$("#quote-text").text(randQuote.quote);
+	$("#author").text(randQuote.author);
+}
+
 $(function() {
-	
+	getQuotes().then(
+		()=>{
+			getQuote();
+		}
+	);	
+
+	$("#new-quote").click(getQuote);
+
 // Sent out the tweet on click.	
 	const tweet = "https://twitter.com/intent/tweet?text=";
 	$("#tweet-quote").click(function(){
